a57621f98659e89ad106cd96cc1458e991b29d9b
Julio Centeno <129196731+Dasso14@users.noreply.github.com>
2024-09-05 18:17:52 -0500

    Add files via upload

---
+++ Practica N°4/Actividades/experiencia1/main.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').onclick = function() {
+    alert('¡Hola! Este es un mensaje de alerta.');
+};
+
+document.getElementById('demo').ondblclick = function() {
+    this.style.color = 'red';
+};
